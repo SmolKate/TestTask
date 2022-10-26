c219b3d4a3435c5b00a630ae5da00406a27301d9
SmolKate <Zdobnovaea@gmail.com>
2022-10-26 15:16:23 +0300

    Fix of problem with User page for nonexistent user introduced through url

--- src/component/user.js
+++ src/component/user.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useParams, Navigate} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import Header from './header';
 import { store } from '../index';
 import NotFound from './notfound';
@@ -9,10 +9,15 @@ function User () {
     
     // Getting of information about thee user saved into the local storage
     const params = useParams(); 
+    const auth = store.getState().auth;
     const exUsers = JSON.parse(localStorage.getItem('redux_store')).users;
     const UserInfo = exUsers.filter(user => {if (user.username == params.username) return user});
+    if (UserInfo.length == 0) {
+        params.username = '';
+
+        return <div><NotFound name={auth} /></div>
+    }
     const email = UserInfo.map(user => user.email);
-    const auth = store.getState().auth;
 
     return (
         <div>
